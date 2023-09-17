a7915dba2b6491917dd906e9fdd5070ab40a96c9
Jonabadie <abadiejoinn@hotmail.fr>
2023-09-17 17:25:50 +0200

    Show all pdf pages + outline

--- src/app/pdf/page.js
+++ src/app/pdf/page.js
@@ -1,28 +1,36 @@
 'use client'
 
 import React, { useState } from 'react';
-import { Page, Document, pdfjs } from 'react-pdf';
+import { Page, Document, pdfjs, Outline } from 'react-pdf';
 import 'react-pdf/dist/Page/TextLayer.css'
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
-export default function Home({ pdfUrl }) {
+export default function Home({ pdfUrl='https://arxiv.org/pdf/1706.03762.pdf' }) {
     const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
+    const [pages, setPages] = useState([]);
+
+    function createPages(numPages) {
+        return Array.from({ length: numPages }, (_, i) => (
+            <Page key={`page_${i + 1}`} pageNumber={i + 1} />
+        ));
+    }
 
     function onDocumentLoadSuccess({ numPages }) {
+        const pages = createPages(numPages);
         setNumPages(numPages);
+        setPages(pages);
     }
 
     return (
         <div>
             <Document
-                file="https://arxiv.org/pdf/1706.03762.pdf"
+                file={pdfUrl}
                 onLoadSuccess={onDocumentLoadSuccess}
             >
-                <Page pageNumber={pageNumber} />
+                <Outline />
+                {pages}
             </Document>
-            <p>Page {pageNumber} of {numPages}</p>
         </div>
     );
 }
